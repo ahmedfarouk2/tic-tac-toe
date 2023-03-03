229ea2088e98a864fc4137c1f228f407c0210f84
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-03-04 00:08:18 +0200

    Logic finished

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const parentContainer = document.querySelector('.parent-container');
 const playBoxes = document.getElementsByClassName('box');
 const playerOneTurn = document.querySelector('.player-1');
 const playerTwoTurn = document.querySelector('.player-2');
-const vsPlayerOption = document.querySelector('.vs-player');
+const startButton = document.querySelector('.start-game');
 const resetButton = document.querySelector('.reset-button');
+const winnerSpot = document.querySelector('.winner-spot');
+const gameSound = document.querySelector('.game-music');
+const buttonSound = document.querySelector('.button-sound');
+const allButtons = document.querySelectorAll('button');
+const gameplaySound = document.querySelector('.gameplay-sound');
+const introMessage = document.querySelector('.intro');
+const enterButton = document.querySelector('.enter-button');
+const winSound = document.querySelector('.yay');
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
@@ -13,6 +22,24 @@ const box7 = document.querySelector('.box7');
 const box8 = document.querySelector('.box8');
 const box9 = document.querySelector('.box9');
 let count = 0;
+let standard = false;
+
+function buttonMusic() {
+  buttonSound.play();
+  gameSound.pause();
+  gameSound.currentTime = 0;
+}
+
+function loadGame() {
+  introMessage.style.opacity = '1';
+}
+
+function enterMenu() {
+  introMessage.style.opacity = '0';
+  parentContainer.classList.add('enter');
+  gameSound.play();
+  setTimeout(() => introMessage.style.display = 'none', 3500);
+}
 
 function gameLogic(e) {
   playerOneTurn.style.color = 'black';
@@ -20,8 +47,10 @@ function gameLogic(e) {
   for (let i = 0; i < playBoxes.length; i++) {
     // eslint-disable-next-line no-loop-func
     playBoxes[i].addEventListener('click', () => {
+      gameplaySound.play();
+      gameplaySound.currentTime = 0;
       if (playBoxes[i].innerText.includes('X') || playBoxes[i].innerText.includes('O')) {
-        return 'oza';
+        return '';
       }
 
       count += 1;
@@ -38,11 +67,37 @@ function gameLogic(e) {
         playerTwoTurn.style.color = 'white';
         playerTwoTurn.style.backgroundColor = 'black';
       }
+      playerWinAnnounce();
     });
   }
 }
 
+function playerWinAnnounce() {
+  if ((box1.innerText === 'X' && box2.innerText === 'X' && box3.innerText === 'X') || (box1.innerText === 'X' && box4.innerText === 'X' && box7.innerText === 'X') || (box3.innerText === 'X' && box6.innerText === 'X' && box9.innerText === 'X') || (box7.innerText === 'X' && box8.innerText === 'X' && box9.innerText === 'X') || (box3.innerText === 'X' && box5.innerText === 'X' && box7.innerText === 'X') || (box2.innerText === 'X' && box5.innerText === 'X' && box8.innerText === 'X') || (box4.innerText === 'X' && box5.innerText === 'X' && box6.innerText === 'X') || (box1.innerText === 'X' && box5.innerText === 'X' && box9.innerText === 'X')) {
+    winnerSpot.innerText = 'Player 1 wins!';
+    winSound.play();
+    setTimeout(() => {
+      resetGameLogic();
+    }, 3000);
+    standard = true;
+  } else if ((box1.innerText === 'O' && box2.innerText === 'O' && box3.innerText === 'O') || (box1.innerText === 'O' && box4.innerText === 'O' && box7.innerText === 'O') || (box3.innerText === 'O' && box6.innerText === 'O' && box9.innerText === 'O') || (box7.innerText === 'O' && box8.innerText === 'O' && box9.innerText === 'O') || (box3.innerText === 'O' && box5.innerText === 'O' && box7.innerText === 'O') || (box2.innerText === 'O' && box5.innerText === 'O' && box8.innerText === 'O') || (box4.innerText === 'O' && box5.innerText === 'O' && box6.innerText === 'O') || (box1.innerText === 'O' && box5.innerText === 'O' && box9.innerText === 'O')) {
+    winnerSpot.innerText = 'Player 2 wins!';
+    setTimeout(() => {
+      winSound.play();
+      resetGameLogic();
+    }, 3000);
+    standard = true;
+  }
+  for (let i = 0; i < playBoxes.length; i++) {
+    if (box1.innerText !== '' && box2.innerText !== '' && box3.innerText !== '' && box4.innerText !== '' && box5.innerText !== '' && box6.innerText !== '' && box7.innerText !== '' && box8.innerText !== '' && box9.innerText !== '' && standard === false) {
+      winnerSpot.innerText = 'It\'s a draw!';
+    }
+  }
+}
+
 function resetGameLogic(e) {
+  standard = false;
+  winnerSpot.innerText = '';
   count = 0;
   for (let i = 0; i < playBoxes.length; i++) {
     playBoxes[i].innerText = '';
@@ -54,5 +109,11 @@ function resetGameLogic(e) {
   e.stopPropagation();
 }
 
+for (let i = 0; i < allButtons.length; i++) {
+  allButtons[i].addEventListener('click', buttonMusic);
+}
+
+window.addEventListener('load', loadGame);
+enterButton.addEventListener('click', enterMenu);
 resetButton.addEventListener('click', resetGameLogic);
-vsPlayerOption.addEventListener('click', gameLogic);
+startButton.addEventListener('click', gameLogic);
