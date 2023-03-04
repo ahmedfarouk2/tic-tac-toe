6fa0219d9a9daf134ffad329e9cc1a8bbc03be82
ahmedfarouk2 <ahmedoza12@gmail.com>
2023-03-04 17:03:03 +0200

    Minor bug fixes and logic improvement

--- app.js
+++ app.js
@@ -5,13 +5,8 @@ const playerTwoTurn = document.querySelector('.player-2');
 const startButton = document.querySelector('.start-game');
 const resetButton = document.querySelector('.reset-button');
 const winnerSpot = document.querySelector('.winner-spot');
-const gameSound = document.querySelector('.game-music');
-const buttonSound = document.querySelector('.button-sound');
-const allButtons = document.querySelectorAll('button');
-const gameplaySound = document.querySelector('.gameplay-sound');
 const introMessage = document.querySelector('.intro');
 const enterButton = document.querySelector('.enter-button');
-const winSound = document.querySelector('.yay');
 const box1 = document.querySelector('.box1');
 const box2 = document.querySelector('.box2');
 const box3 = document.querySelector('.box3');
@@ -24,12 +19,6 @@ const box9 = document.querySelector('.box9');
 let count = 0;
 let standard = false;
 
-function buttonMusic() {
-  buttonSound.play();
-  gameSound.pause();
-  gameSound.currentTime = 0;
-}
-
 function loadGame() {
   introMessage.style.opacity = '1';
 }
@@ -37,18 +26,18 @@ function loadGame() {
 function enterMenu() {
   introMessage.style.opacity = '0';
   parentContainer.classList.add('enter');
-  gameSound.play();
-  setTimeout(() => introMessage.style.display = 'none', 3500);
+  setTimeout(() => introMessage.style.display = 'none', 1000);
 }
 
 function gameLogic(e) {
+  startButton.style.display = 'none'
   playerOneTurn.style.color = 'black';
   playerOneTurn.style.backgroundColor = 'white';
+  playerTwoTurn.style.color = 'white';
+  playerTwoTurn.style.backgroundColor = 'black';
   for (let i = 0; i < playBoxes.length; i++) {
     // eslint-disable-next-line no-loop-func
     playBoxes[i].addEventListener('click', () => {
-      gameplaySound.play();
-      gameplaySound.currentTime = 0;
       if (playBoxes[i].innerText.includes('X') || playBoxes[i].innerText.includes('O')) {
         return '';
       }
@@ -75,16 +64,12 @@ function gameLogic(e) {
 function playerWinAnnounce() {
   if ((box1.innerText === 'X' && box2.innerText === 'X' && box3.innerText === 'X') || (box1.innerText === 'X' && box4.innerText === 'X' && box7.innerText === 'X') || (box3.innerText === 'X' && box6.innerText === 'X' && box9.innerText === 'X') || (box7.innerText === 'X' && box8.innerText === 'X' && box9.innerText === 'X') || (box3.innerText === 'X' && box5.innerText === 'X' && box7.innerText === 'X') || (box2.innerText === 'X' && box5.innerText === 'X' && box8.innerText === 'X') || (box4.innerText === 'X' && box5.innerText === 'X' && box6.innerText === 'X') || (box1.innerText === 'X' && box5.innerText === 'X' && box9.innerText === 'X')) {
     winnerSpot.innerText = 'Player 1 wins!';
-    winSound.play();
-    winSound.currentTime = 0;
     setTimeout(() => {
       resetGameLogic();
     }, 3000);
     standard = true;
   } else if ((box1.innerText === 'O' && box2.innerText === 'O' && box3.innerText === 'O') || (box1.innerText === 'O' && box4.innerText === 'O' && box7.innerText === 'O') || (box3.innerText === 'O' && box6.innerText === 'O' && box9.innerText === 'O') || (box7.innerText === 'O' && box8.innerText === 'O' && box9.innerText === 'O') || (box3.innerText === 'O' && box5.innerText === 'O' && box7.innerText === 'O') || (box2.innerText === 'O' && box5.innerText === 'O' && box8.innerText === 'O') || (box4.innerText === 'O' && box5.innerText === 'O' && box6.innerText === 'O') || (box1.innerText === 'O' && box5.innerText === 'O' && box9.innerText === 'O')) {
     winnerSpot.innerText = 'Player 2 wins!';
-    winSound.play();
-    winSound.currentTime = 0;
     setTimeout(() => {
       resetGameLogic();
     }, 3000);
@@ -111,11 +96,7 @@ function resetGameLogic(e) {
   e.stopPropagation();
 }
 
-for (let i = 0; i < allButtons.length; i++) {
-  allButtons[i].addEventListener('click', buttonMusic);
-}
-
 window.addEventListener('load', loadGame);
 enterButton.addEventListener('click', enterMenu);
 resetButton.addEventListener('click', resetGameLogic);
-startButton.addEventListener('click', gameLogic);
+startButton.addEventListener('click', gameLogic, {once:true});
